c79b41bbe1846d7263047e60ea1216164f997d44
Erik D <erik.d.dev@gmail.com>
2015-10-29 13:52:10 -0400

    Completed test interface and constants class

--- SpriteInterface/src/spriteInterface/Constants.java
+++ SpriteInterface/src/spriteInterface/Constants.java
@@ -2,5 +2,5 @@ package spriteInterface;
 
 public class Constants {
 	public static final String RMI_ID = "SpriteRMI";
-	public static final int RMI_PORT = 8081;
+	public static final int RMI_PORT = 222;
 }
--- SpriteInterface/src/spriteInterface/SpriteInterface.java
+++ SpriteInterface/src/spriteInterface/SpriteInterface.java
@@ -1,7 +1,8 @@
 package spriteInterface;
 
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface SpriteInterface {
+public interface SpriteInterface extends Remote {
 	public boolean test(String username) throws RemoteException;
 }
